00457bd58fc432ea89ba8ee337a5cf7849ac7ff1
Evaldas Stankus <evalsta@gmail.com>
2017-12-20 19:41:26 +0200

    Bandymas ar veikia lentele

--- js/script.js
+++ js/script.js
@@ -33,7 +33,20 @@ document.getElementById('SiustiDuomenis').addEventListener('click', function (ev
     }
 });
 
-var table = document.getElementById("table");
-var row = table.insertRow();
-var vardas = row.insertCell(0);
+function myFunction() {
+    var table = document.getElementById("table");
+    var row = table.insertRow();
+    var vardas = row.insertCell(0);
+    var pavarde = row.insertCell(1);
+    var gimimoMetai = row.insertCell(2);
+    var telNumeris = row.insertCell(3);
+    var elPastas = row.insertCell(4);
+    var adresas = row.insertCell(5);
+    vardas.innerHTML = document.forms['myForm']['Vardas'];
+    pavarde.innerHTML = document.forms['myForm']['Pavarde'];
+    gimimoMetai.innerHTML = document.forms['myForm']['GimimoMetai'];
+    telNumeris.innerHTML = document.forms['myForm']['TelNumeris'];
+    elPastas.innerHTML = document.forms['myForm']['ElPastas'];
+    adresas.innerHTML = document.forms['myForm']['Adresas'];
+};
 
